0539efcf0bebc4130385340fd00f882e26cbc8f5
Hunter Grubbs <hgrubbs@momentumsi.com>
2014-08-14 10:48:47 -0400

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+## YAML Validator ##
+
+yaml_validate is a short Python program to validate and print(using pp.PrettyPrinter()) YAML.
---
+++ yaml_validate.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Short program to validate and prettyprint parsed YAML, by Hunter Grubbs <hgrubbs@momentumsi.com>
+
+import sys
+try:
+  import yaml
+  import pprint
+except Exception as e:
+  print("Python modules 'yaml' and 'pprint' must both be installed/importable!")
+  print("Error: %s" % (e))
+  sys.exit(1)
+
+if len(sys.argv) < 1:
+  print("Usage: yaml_validate <input-file>")
+  sys.exit(1)
+
+input_file = sys.argv[1]
+
+try:
+  unparsed_yaml = open(input_file).read()
+except Exception as e:
+  print("Failed to read %s!" % (input_file))
+  print("Error: %s" % (e))
+  sys.exit(1)
+
+try:
+  parsed_yaml = yaml.load(unparsed_yaml)
+except Exception as e:
+  print("Failed to parse %s!" % (input_file))
+  print("Error: %s" % (e))
+  sys.exit(1)
+
+pp = pprint.PrettyPrinter()
+pp.pprint(parsed_yaml)
